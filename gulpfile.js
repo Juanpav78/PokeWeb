76d7af0fc6bc9cdb6a24f6a8b1c669b586acbdb6
Juanpav78 <juanpav.78d@gmail.com>
2022-07-05 21:13:37 -0500

    Añadiendo el FrontEnd v.01

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,48 @@
 'use strict';
-const { series } = require('gulp');
-const gulp = require('gulp');
+const {src, dest, watch, parallel, series} = require('gulp');
+ 
+ 
+//css
 const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+//js
+const autoPrefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser-js');
 
-function buildStyles() {
-    return gulp.src('./sass/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
-};
-
-function watch(){
-    gulp.watch('./sass/**/*.scss', ['sass']);
+const path = {
+    scss: 'src/scss/**/*.scss',
+    css: 'build/css/app.css',
+    js: 'src/js/**/*.js',
+}
+ 
+ 
+function compileSass() {
+    return src(path.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoPrefixer(),cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'));
+}
+ 
+function compileJS(){
+    return src(path.js)
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/js'));
+}
+ 
+ 
+ 
+function autoCompile(){
+    watch(path.scss,compileSass);
+    watch(path.js, compileJS);
 }
 
-exports.default = series(buildStyles, watch);
+exports.default = series(compileSass,compileJS,autoCompile);
+exports.autoCompile = autoCompile;
